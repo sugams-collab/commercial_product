45e10d4759b93eedf7c30e863462fc33fff7c6ae
sugams-collab <sugam.s@cisinlabs.com>
2026-05-19 14:52:05 +0530

    runtimr chnage python

--- backend/run.py
+++ backend/run.py
@@ -4,4 +4,4 @@ from importlib import import_module
 import uvicorn
 
 if __name__ == "__main__":
-    uvicorn.run("app.main:app", host="127.0.0.1", port=8000, reload=True)
+    uvicorn.run("app.main:app", host="127.0.0.1", port=8000)
