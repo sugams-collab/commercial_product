b6c0b6a28e612250467f31be76b21f8aad045df4
sugams-collab <sugam.s@cisinlabs.com>
2026-05-19 15:11:35 +0530

    fix port issu

--- backend/run.py
+++ backend/run.py
@@ -7,4 +7,4 @@ import os
 if __name__ == "__main__":
     port = int(os.environ.get("PORT", 8000))
 
-    uvicorn.run("backend.app.main:app", host="0.0.0.0", port=port)
+    uvicorn.run("app.main:app", host="0.0.0.0", port=port)
